e7b71d99962d45860e741f70cf2c9db5ea07a610
Klurien <202581234+Klurien@users.noreply.github.com>
2026-03-19 15:00:31 +0300

    fix: import path module in controllers

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,6 +1,7 @@
 import db from '../db/db.js';
 import { uploadFile, deleteFile } from '../utils/upload.js';
 import { fileURLToPath } from 'url';
+import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
